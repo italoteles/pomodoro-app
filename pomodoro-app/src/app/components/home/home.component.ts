6cdf08de9424c2b45c0e656633c8c5e009a0009c
Italo Teles <italop.teles@gmail.com>
2023-09-27 16:25:18 -0300

    animação do circulo de progresso

--- pomodoro-app/src/app/components/home/home.component.ts
+++ pomodoro-app/src/app/components/home/home.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, Subscription, map, takeWhile, tap, timer } from 'rxjs';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,AfterViewInit {
+
 
 
   statusLabel : string = 'PAUSE';
   timerType : string = 'pomodoro'
-  minutesConfigPomodoro : number = 25;
+  minutesConfigPomodoro : number = 2;
   minutesConfigShort : number = 5;
   minutesConfigLong : number = 15;
   secondsRemaing! : number;
   status : string = 'notStarted';
+  dashArrayNumber : number = 0;
+  dashOffSetNumber : number = 0;
+
+  @ViewChild("circleProgress") circleProgress!: ElementRef;
 
 
   timerId! : any;
@@ -23,12 +28,20 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.secondsRemaing = this.minutesConfigPomodoro * 60;
+
+  }
+
+  ngAfterViewInit(): void {
+
+    this.dashArrayNumber = this.circleProgress.nativeElement.r.baseVal.value * 3.14 * 2
+
   }
 
   changeTypeTimer(type : string){
     this.timerType = type;
     clearInterval(this.timerId)
     this.status = 'notStarted';
+    this.dashOffSetNumber = 0;
     if (this.timerType=='pomodoro'){
       this.secondsRemaing = this.minutesConfigPomodoro * 60;
     } else{
@@ -49,6 +62,7 @@ export class HomeComponent implements OnInit {
       this.status = 'started';
       this.timerId = setInterval(() => {
         this.secondsRemaing = this.secondsRemaing - 1;
+        this.calculateProgress();
       }, 1000);
 
     }else{
@@ -60,11 +74,30 @@ export class HomeComponent implements OnInit {
         this.status = 'started';
         this.timerId = setInterval(() => {
           this.secondsRemaing = this.secondsRemaing - 1;
+          this.calculateProgress();
         }, 1000);
       }
     }
 
 
+  }
+
+  calculateProgress(){
+    let total : number = 0;
+    if (this.timerType=='pomodoro'){
+      total = this.minutesConfigPomodoro * 60;
+    } else{
+      if (this.timerType=='short'){
+        total = this.minutesConfigShort * 60;
+      }
+      else{
+        total = this.minutesConfigLong * 60;
+      }
+    }
+
+    let percentProgress = this.secondsRemaing / total;
+
+    this.dashOffSetNumber = this.dashArrayNumber *percentProgress;
   }
 
 
